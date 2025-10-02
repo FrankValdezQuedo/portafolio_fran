7a4afca64bc0bdff4520b32f8eefafbecd10f090
FrankValdezQuedo <109559378+FrankValdezQuedo@users.noreply.github.com>
2025-10-02 08:48:54 -0500

    ok

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,18 +1,18 @@
 const Experience = () => {
   const experiences = [
     {
-      title: "Ingeniero DevOps",
-      company: "Redondos S.A.",
-      period: "Enero 2024 - Julio 2024",
-      description:
-        "Encargado de la integración y entrega continua mediante Jenkins, automatización de despliegues con Ansible, y análisis de calidad de código con SonarQube. Gestión de artefactos en Nexus, control de versiones con Bitbucket y administración de servidores con Weblogic.",
-    },
-    {
       title: "Desarrollador Backend",
       company: "FLCode",
-      period: "Enero 2025 - Agosto 2025",
+      period: "Noviembre 2024 - Agosto 2025",
       description:
         "Desarrollé APIs RESTful usando Spring Boot y WebFlux dentro de una arquitectura hexagonal, aplicando principios de diseño limpio y despliegue basado en microservicios.",
+    },
+    {
+      title: "Ingeniero DevOps",
+      company: "Redondos S.A.",
+      period: "Julio 2024 - Diciembre 2024",
+      description:
+        "Encargado de la integración y entrega continua mediante Jenkins, automatización de despliegues con Ansible, y análisis de calidad de código con SonarQube. Gestión de artefactos en Nexus, control de versiones con Bitbucket y administración de servidores con Weblogic.",
     },
   ];
 
